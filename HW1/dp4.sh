1fb4d3e8e2afb39ac601e0063bf470fa290cc0d3
syalexandra <sunyuealexandra@gmail.com>
2023-02-11 12:56:37 -0500

    add new files

---
+++ HW1/dp4.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Simple "Hello World" submit script for Slurm.
+#
+# Replace <ACCOUNT> with your account name before submitting.
+#
+#SBATCH --account=ys3535 # The account name for the job.
+#SBATCH --job-name=HelloWorld # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=1:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
+echo ".dp1 1000000 1000"
