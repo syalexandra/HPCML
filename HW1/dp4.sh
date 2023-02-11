f9417abb4b7d67251df2b6d5fd44150f8003b24c
syalexandra <sunyuealexandra@gmail.com>
2023-02-11 13:04:41 -0500

    add new files

--- HW1/dp4.sh
+++ HW1/dp4.sh
@@ -4,9 +4,12 @@
 #
 # Replace <ACCOUNT> with your account name before submitting.
 #
-#SBATCH --account=ys3535
-#SBATCH --job-name=dp4
-#SBATCH -c 1
-#SBATCH --time=1:00
-#SBATCH --mem-per-cpu=1gb
-python dp4.py 300000000 20
+#SBATCH --account=ys3535 # The account name for the job.
+#SBATCH --job-name=HelloWorld # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=1:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
+echo "Hello World"
+sleep 10
+date
+# End of script
