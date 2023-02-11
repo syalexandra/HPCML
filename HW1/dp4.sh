e6aa7aafbf8dd5c4c070eeeb76095168c8e76130
syalexandra <sunyuealexandra@gmail.com>
2023-02-11 12:58:11 -0500

    add new files

--- HW1/dp4.sh
+++ HW1/dp4.sh
@@ -5,8 +5,8 @@
 # Replace <ACCOUNT> with your account name before submitting.
 #
 #SBATCH --account=ys3535 # The account name for the job.
-#SBATCH --job-name=HelloWorld # The job name.
+#SBATCH --job-name=dp4 # The job name.
 #SBATCH -c 1 # The number of cpu cores to use.
 #SBATCH --time=1:00 # The time the job will take to run.
 #SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
-echo ".dp1 1000000 1000"
+echo "./dp1 1000000 1000"
