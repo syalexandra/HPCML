26e169f6ee934cdb9c145b67235cbed5a109c70e
syalexandra <sunyuealexandra@gmail.com>
2023-02-11 13:02:05 -0500

    add new files

--- HW1/dp4.sh
+++ HW1/dp4.sh
@@ -9,4 +9,4 @@
 #SBATCH -c 1 # The number of cpu cores to use.
 #SBATCH --time=1:00 # The time the job will take to run.
 #SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
-echo "./dp1 1000000 1000"
+python dp4.py 300000000 20
