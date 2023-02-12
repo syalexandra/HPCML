69ba75ce0bbcedf203f76472ea80978b58a6d094
syalexandra <sunyuealexandra@gmail.com>
2023-02-12 18:40:03 -0500

    add new file

--- HW1/dp4.sh
+++ HW1/dp4.sh
@@ -9,7 +9,5 @@
 #SBATCH -c 1 # The number of cpu cores to use.
 #SBATCH --time=1:00 # The time the job will take to run.
 #SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
-echo "Hello World"
-sleep 10
-date
+srun ./dp3 1000000 1000
 # End of script
