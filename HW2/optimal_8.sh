42519c8b698c9aa93d982b119a6f76256f31843d
syalexandra <sunyuealexandra@gmail.com>
2023-02-21 21:53:54 -0500

    add batch files

---
+++ HW2/optimal_8.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Simple "Hello World" submit script for Slurm.
+#
+# Replace <ACCOUNT> with your account name before submitting.
+#
+#SBATCH --account=edu # The account name for the job.
+#SBATCH --job-name=FindOptimalWorker_8 # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=1:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
+module load anaconda
+python main.py --num-of-workers 8
+# End of script
